7820a5a69eff26b8c132bb1e4d6c103f7c3f4059
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-04-08 02:52:36 +0530

    Added input validation

--- main.go
+++ main.go
@@ -28,10 +28,36 @@ func main() {
 	fmt.Scan(&toUnit)
 	toUnit = strings.ToUpper(toUnit)
 
+	fromUnit = readValidTemperatureUnit("Enter the current unit (C, K, F): ")
+	toUnit = readValidTemperatureUnit("Enter the unit to convert to (C, K, F): ")
+
+
 	convertedTemp := convertTemp(temp, fromUnit, toUnit)
 
 	fmt.Printf("%.2f %s is %.2f %s\n", temp, fromUnit, convertedTemp, toUnit)
 
+}
+
+func readValidTemperatureUnit(prompt string) string {
+	var unit string
+	for {
+		fmt.Println(prompt)
+		fmt.Scanln(&unit)
+		unit = strings.ToUpper(unit)
+		if isValidTemperatureUnit(unit) {
+			return unit
+		}
+		fmt.Println("Invalid temperature unit. Please enter a valid unit (C, K, F).")
+	}
+}
+
+func isValidTemperatureUnit(unit string) bool {
+	switch unit {
+	case Celsius, Kelvin, Fahrenheit:
+		return true
+	default:
+		return false
+	}
 }
 
 func convertTemp(temp float64, fromUnit, toUnit string) float64 {
